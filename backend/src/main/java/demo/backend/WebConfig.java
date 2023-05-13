7bef122b604b19342c5f506298a3d697e6871883
IsmaQty <ismaqty33@gmail.com>
2023-05-12 21:36:45 -0300

    Agrego archivo Docker, y archivo de configuracion CORS

---
+++ backend/src/main/java/demo/backend/WebConfig.java
@@ -0,0 +1,20 @@
+package demo.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * Clase que habilita CORS
+ * @author YOProgramo - Ismael
+ */
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
